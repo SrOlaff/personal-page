97473f7cc21072c6cd77fc959671118fd751e743
SrOlaff <polavo00@gmail.com>
2022-08-03 18:08:38 -0300

    chore: tests github config

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="w-screen h-screen bg-black">
-      Hello World
+    <div className="w-screen h-screen bg-background flex justify-center items-center">
+        <h1 className="text-accent text-center text-6xl font-bold ">Hello World!</h1>
     </div>
   );
 }
