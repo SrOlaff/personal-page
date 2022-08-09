0b372d8e4687e43d79c7a79ffc01bcaa4c45671a
SrOlaff <polavo00@gmail.com>
2022-08-08 23:03:06 -0300

    feat: adds Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <nav className="w-full fixed">
+      <ul className="container mx-auto text-white flex justify-end space-x-14 text-2xl py-8">
+        <li>home</li>
+        <li>me</li>
+        <li>contact</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Header
