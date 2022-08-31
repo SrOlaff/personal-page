f61642070bb99ede03f7789b8f626f3c8b5c613c
SrOlaff <polavo00@gmail.com>
2022-08-30 21:00:13 -0300

    feat: adds blurred div when mobile aside is in view

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import { motion } from "framer-motion";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import Home from "./screens/Home";
 
 function App() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
   return (
-    <div className="bg-background w-screen h-screen">
-      <Header />
-      <Home />
+    <div className="w-full bg-background">
+      <Header
+        isOpen={isMobileMenuOpen}
+        toggle={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+      />
+
+      <motion.div
+        className={`transition-all duration-300 md:blur-0 ${
+          isMobileMenuOpen && "blur"
+        }`}
+      >
+        <div className="h-screen w-screen bg-backgroudn flex justify-center items-center">
+          <h1>Hello World</h1>
+        </div>
+      </motion.div>
     </div>
   );
 }
