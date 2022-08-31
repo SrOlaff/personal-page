dd24c971a0e856793c5dc771ef786aa65272e352
SrOlaff <polavo00@gmail.com>
2022-08-30 23:26:51 -0300

    chore: removes unused hook

--- src/components/Header/hooks/useIsMenuOpen.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function useIsMenuOpen() {
-  const [isOpen, setIsOpen] = React.useState(false);
-
-  const toggle = () => setIsOpen(!isOpen);
-
-  return { isOpen, toggle };
-}
