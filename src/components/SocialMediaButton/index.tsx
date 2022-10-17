9adbf961ae930c065c1aa8ba9b46cabdb662d868
SrOlaff <polavo00@gmail.com>
2022-10-17 19:55:51 -0300

    feat: adds links and responsivity to SocialMediaButton

--- src/components/SocialMediaButton/index.tsx
+++ src/components/SocialMediaButton/index.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { IconBaseProps, IconType } from "react-icons";
+import { IconBaseProps } from "react-icons";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const SOCIAL_MEDIA_ICONS = {
-  GITHUB: {
+  github: {
     Component: (props?: IconBaseProps) => <FaGithub {...props} />,
-    link: "https://google.com",
+    link: "https://github.com/SrOlaff",
   },
-  LINKEDIN: {
+  linkedin: {
     Component: (props?: IconBaseProps) => <FaLinkedin {...props} />,
-    link: "https://google.com",
+    link: "https://linkedin.com/in/pedroolavo",
   },
 };
 
-type SocialMediaIcons = "GITHUB" | "LINKEDIN";
+type SocialMediaIcons = "github" | "linkedin";
 
 interface SocialMediaButtonProps {
   iconName: SocialMediaIcons;
@@ -21,14 +21,15 @@ interface SocialMediaButtonProps {
 
 function SocialMediaButton({ iconName }: SocialMediaButtonProps) {
   const SocialMediaIcon = SOCIAL_MEDIA_ICONS[iconName];
+
   return (
     <a
       href={SocialMediaIcon.link}
       target="_blank"
       rel="noopener noreferrer"
-      className="flex justify-center items-center p-4 rounded-full bg-surface hover:opacity-70 transition-all duration-200"
+      className="flex justify-center items-center p-4 2xl:p-5 mr-3 rounded-full bg-surface hover:opacity-70 transition-all duration-200"
     >
-      <SocialMediaIcon.Component className="text-text text-2xl" />
+      <SocialMediaIcon.Component className="text-text text-2xl 2xl:text-3xl" />
     </a>
   );
 }
