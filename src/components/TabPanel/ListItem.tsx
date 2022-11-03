9b184a0e3cf4b5a32f181fb66c7865682f856aad
SrOlaff <polavo00@gmail.com>
2022-11-02 22:28:51 -0300

    refactor: refactors ListItem

--- src/components/TabPanel/ListItem.tsx
+++ src/components/TabPanel/ListItem.tsx
@@ -9,13 +9,13 @@ interface ListItemProps {
 function ListItem({ title, isSelected, onClick }: ListItemProps) {
   return (
     <li
-      className={`hover:bg-surface flex flex-1 lg:flex-none lg:h-1/6 border-b lg:border-b-0 lg:border-r transition-all duration-200 ${
-        isSelected && "bg-surface border-accent text-accent"
+      className={`text-text hover:bg-surface flex flex-1 lg:flex-none lg:h-1/6 border-b lg:border-b-0 lg:border-r transition-all duration-200 ${
+        isSelected && "bg-surface border-accent text-accent "
       } `}
     >
       <button
         type="button"
-        className="flex flex-1 items-center justify-center px-2 font-medium text-lg transition-all duration-200"
+        className="flex flex-1 items-center justify-center px-2 font-medium text-lg xl:text-xl 2xl:text-2xl transition-all duration-200"
         onClick={onClick}
       >
         {title}
