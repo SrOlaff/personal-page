6dab28243579e02ac9d29ea87701f25d10e60cc7
pedro-olavo <polavo00@gmail.com>
2022-11-22 21:31:39 -0300

    feat: adds force active in Link component

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link as LinkBase, LinkProps as LinkBaseProps } from "react-scroll";
 
-type LinkProps = Omit<LinkBaseProps, "ref">;
+type LinkProps = Omit<LinkBaseProps, "ref"> & {
+  forceActive?: boolean;
+};
 
-function Link({ children, ...rest }: LinkProps) {
+function Link({ children, forceActive, ...rest }: LinkProps) {
   return (
     <LinkBase
-      className={`cursor-pointer transition-colors duration-200 ${rest.className}`}
+      className={`cursor-pointer transition-colors duration-200
+      ${forceActive && "active"}`}
       smooth
       spy
       {...rest}
