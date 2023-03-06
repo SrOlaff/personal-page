57f02139a88d3e7703bb7919d80aeb65f2e1ce05
pedro-olavo <polavo00@gmail.com>
2023-03-06 18:49:55 -0300

    chore: changes about me text

--- src/screens/Me/index.tsx
+++ src/screens/Me/index.tsx
@@ -23,14 +23,14 @@ function Me() {
         className="mt-4 px-5 text-text lg:mt-16"
         variants={typographyAnimations}
       >
-        <h2 className="mb-2 w-fit border-b border-accent text-3xl font-bold sm:text-4xl lg:text-5xl 2xl:text-6xl md:portrait:text-5xl">
+        <h2 className="mb-2 w-fit border-b border-accent text-3xl font-bold sm:text-4xl md:portrait:text-5xl lg:text-5xl 2xl:text-6xl">
           me
         </h2>
         <p className="text-justify md:text-lg xl:text-xl 2xl:text-2xl">
-          Hello! My name is Pedro Olavo, I&apos;m 18 years old and I have worked
-          in the tech industry as a Software developer for a year. Ever since my
-          early days I was deeply passionate about technology, and it has always
-          been my dream to contribute to the upcoming changes in the world.
+          Hello! My name is Pedro Olavo, I&apos;m 19 years old and I work in the
+          tech industry as a software developer. Ever since my early days I was
+          deeply passionate about technology, and it has always been my dream to
+          contribute to the upcoming changes in the world.
         </p>
       </motion.section>
 
@@ -38,7 +38,7 @@ function Me() {
         className="flex flex-1 flex-col xl:max-h-[70vh]"
         variants={typographyAnimations}
       >
-        <h2 className="mx-5 mb-4 w-fit border-b border-accent text-3xl font-bold text-white sm:text-4xl lg:text-5xl 2xl:text-6xl md:portrait:text-5xl">
+        <h2 className="mx-5 mb-4 w-fit border-b border-accent text-3xl font-bold text-white sm:text-4xl md:portrait:text-5xl lg:text-5xl 2xl:text-6xl">
           experience
         </h2>
 
@@ -66,12 +66,9 @@ function Me() {
           variants={checkoutSocialMediaAnimation}
         />
 
-        <motion.div variants={checkoutSocialMediaAnimation}>
-          <SocialMediaButton iconName="github" />
-        </motion.div>
-        <motion.div variants={checkoutSocialMediaAnimation}>
-          <SocialMediaButton iconName="linkedin" />
-        </motion.div>
+        <SocialMediaButton iconName="github" />
+
+        <SocialMediaButton iconName="linkedin" />
       </motion.section>
     </motion.div>
   );
